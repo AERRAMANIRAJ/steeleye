650e366614f311d3a9302bb6be641fd3698e233d
AERRAMANIRAJ <94050132+AERRAMANIRAJ@users.noreply.github.com>
2023-04-23 19:33:31 +0530

    Done

---
+++ components/List.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect, memo } from 'react';
+import PropTypes from 'prop-types';
+
+// Single List Item
+const WrappedSingleListItem = ({ index, isSelected, onClickHandler, text }) => {
+  return (
+    <li
+      style={{ backgroundColor: isSelected ? 'green' : 'red' }}
+      onClick={onClickHandler(index)}
+    >
+      {text}
+    </li>
+  );
+};
+WrappedSingleListItem.propTypes = {
+  index: PropTypes.number,
+  isSelected: PropTypes.bool,
+  onClickHandler: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
+const SingleListItem = memo(WrappedSingleListItem);
+
+// List Component
+const WrappedListComponent = ({ items }) => {
+  //setState parametrer error
+  const [selectedIndex, setSelectedIndex] = useState();
+
+  useEffect(() => setSelectedIndex(true), [items]);
+
+  const handleClick = (index) => {
+    setSelectedIndex(index);
+
+    console.log('Index Value: ' + index);
+  };
+
+  return (
+    <ul style={{ textAlign: 'left' }}>
+      {items.map((item, index) => (
+        <SingleListItem
+          onClickHandler={() => handleClick(index)}
+          text={item.text}
+          index={index}
+          isSelected={selectedIndex}
+          //key is missing
+          key={index}
+        />
+      ))}
+    </ul>
+  );
+};
+
+WrappedListComponent.propTypes = {
+  //It should be arrayOf instead of shapeOf
+
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+//Fixed :[null]
+WrappedListComponent.defaultProps = {
+  items: [
+    { index: 1, text: 'Aerra' },
+    { index: 2, text: 'Maniraj' },
+  ],
+};
+
+const List = memo(WrappedListComponent);
+
+export default List;
